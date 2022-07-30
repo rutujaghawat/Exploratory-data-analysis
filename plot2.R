61e385550f9909c4988a1ad7601dba363bb2d16a
rutujaghawat <102845918+rutujaghawat@users.noreply.github.com>
2022-07-30 14:57:54 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,6 +3,7 @@ power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
+
 # Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
 subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
 subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
